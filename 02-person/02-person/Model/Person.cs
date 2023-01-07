c8918badaac5a3e985c5ba01a4f236dc91724937
Joao Vitor Muniz Lopes <joao.lopes@mundiale.com.br>
2023-01-07 11:55:17 -0300

    feat: add base person project

---
+++ 02-person/02-person/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_person.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public long FirstName { get; set; }
+        public long LastName { get; set; }
+        public long Address { get; set; }
+        public long Gender { get; set; }
+    }
+}
---
+++ 02-person/02-person/Services/Implementations/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using _02_person.Model;
+
+namespace _02_person.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        Person Delete(long id);
+    }
+}
