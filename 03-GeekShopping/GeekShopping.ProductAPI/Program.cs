131ad1479fa5859eba17bc7d7e831bf8bbc6bb1a
Joao Vitor Muniz Lopes <joao.lopes@mundiale.com.br>
2023-01-15 22:49:03 -0300

    feat: add migrations

--- 03-GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ 03-GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,12 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = "MySQlConnection:MySQlConnectionString";
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(builder.Configuration[connectionString], new MySqlServerVersion(new Version(8, 0, 5))));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
